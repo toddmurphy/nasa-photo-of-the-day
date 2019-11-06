613ae6062cf89b64a6b7003cfb85ad71563b6072
toddmurphy <toddmurphy93@gmail.com>
2019-11-06 16:58:22 -0400

    added axios dependency locally and was able to fetch nasa data response successfully

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -6,9 +6,14 @@ const ImageList = () => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=rpCnWzJ5bMqSJf9V6NLhEExKWSNTno3XE8Q6gUs3').then(response => {
-      console.log(response);
-    });
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=rpCnWzJ5bMqSJf9V6NLhEExKWSNTno3XE8Q6gUs3')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log('You did not return any images', error);
+      });
   }, []); //MUST add empty array dependency to avoid infinite loop
 
   return (
