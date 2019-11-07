02a2dfef6e6295f6c5ff032e01588f59a3e1c3d7
toddmurphy <toddmurphy93@gmail.com>
2019-11-07 17:02:55 -0400

    installed styled components and updated styles for title and image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ImageList from './components/ImageList';
+import styled from 'styled-components';
 import './App.css';
+
+const AppTitle = styled.h1`
+  font-size: 3rem;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>NASA: Image of the day</h1>
-
+      <AppTitle>NASA: Image of the day</AppTitle>
       <ImageList />
     </div>
   );
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ImageWrapper = styled.img`
+  width: 40%;
+  /* height: 400px; */
+`;
+
+const ImageContainer = styled.div`
+  border: 2px solid green;
+  width: 50%;
+  margin: auto;
+`;
 
 const Image = props => {
   return (
-    <div>
+    <ImageContainer>
       <h3>Image Title: {props.title}</h3>
-      <img src={props.url} />
+      <ImageWrapper src={props.url} />
       <p>Explanation: {props.explanation}</p>
       <p>Date: {props.date}</p>
-    </div>
+    </ImageContainer>
   );
 };
 
